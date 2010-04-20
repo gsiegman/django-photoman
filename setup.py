e12d768973d6c6e0bc33fd437a2a7577399f644c
Glenn Siegman <gsiegman@gsiegman.com>
2010-04-20 18:02:25 -0500

    initial app creation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-photoman',
+        version='0.1',
+        description='A django photo gallery application. Built with the purpose of allowing flexibility and extensibility.',
+        author='Glenn Siegman',
+        author_email='gsiegman@gsiegman.com',
+        url='http://github.com/gsiegman/django-photoman',
+        packages=['photoman']
+)
